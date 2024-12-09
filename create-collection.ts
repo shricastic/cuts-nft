57d5fcef4985c79b84fed89acd6ca2cfbc4569fd
Shri <shricse07@gmail.com>
2024-12-09 08:12:02 +0530

    init: first commit

---
+++ create-collection.ts
@@ -0,0 +1,63 @@
+import {
+  createNft,
+  fetchDigitalAsset,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL,
+);
+
+console.log("loaded user", user.publicKey.toBase58());
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("set up Umi instance for user");
+
+const collectionMint = generateSigner(umi);
+
+const transaction = await createNft(umi, {
+  mint: collectionMint,
+  name: "Cuts collection",
+  symbol: "CUTS",
+  uri: "https://",
+  sellerFeeBasisPoints: percentAmount(0),
+  isCollection: true,
+});
+
+await transaction.sendAndConfirm(umi);
+
+const createdCollectionNft = await fetchDigitalAsset(
+  umi,
+  collectionMint.publicKey,
+);
+
+console.log(
+  `created collection! address is${getExplorerLink(
+    "address",
+    createdCollectionNft.mint.publicKey,
+    "devnet",
+  )}`,
+);
